ff19ffc65cdaf6cf656e3a7b2e75bced11c5d0c1
torine6 <vicimbuhila2000@gmail.com>
2022-02-21 00:12:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# using nested if...else
+year = int(input('Enter the year: '))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f'The year {year} is a leap year')
+        else:
+            print(f'The year {year} is not a leap year')
+    else:
+        print(f'The year {year} is a leap year')
+else:
+    print(f'The given year {year} is not a leap year')
